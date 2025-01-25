10f488404f9f99333d8c684eb47445eeeb01caf1
Ricardo Branco <rbranco@suse.de>
2025-01-25 12:03:22 +0100

    FreeBSD: Add cap_rights_limit syscall

--- FreeBSD/test.c
+++ FreeBSD/test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/capsicum.h>
 #include <err.h>
 
-int
-main(void) {
+#define USAGE	"Usage: %s cap_enter|cap_rights_limit"
+
+static void
+test_cap_enter(void)
+{
 	u_int mode;
 
 	if (cap_enter() < 0)
@@ -15,6 +21,39 @@ main(void) {
 		errx(1, "Inside sandbox");
 	else
 		printf("PASS\n");
+}
 
-	return (0);
+static void
+test_cap_rights_limit(void)
+{
+	cap_rights_t rights;
+	char buf[BUFSIZ];
+	int fd;
+
+	fd = open("/etc/passwd", O_RDONLY);
+	if (fd == -1)
+		err(1, "open");
+
+	// No CAP_READ
+	(void)cap_rights_init(&rights, CAP_FSTAT);
+
+	if (cap_rights_limit(fd, &rights) == -1)
+		err(1, "cap_rights_limit");
+
+	if (read(fd, buf, sizeof(buf)) == -1)
+		err(1, "read");
+
+	(void)close(fd);
+}
+
+int
+main(int argc, char *argv[]) {
+	if (!**argv || argc != 2)
+		errx(1, USAGE, getprogname());
+	if (!strcmp(argv[1], "cap_enter"))
+		test_cap_enter();
+	else if (!strcmp(argv[1], "cap_rights_limit"))
+		test_cap_rights_limit();
+	else
+		errx(1, USAGE, getprogname());
 }
--- FreeBSD/unsandbox.c
+++ FreeBSD/unsandbox.c
@@ -3,3 +3,9 @@ cap_enter(void)
 {
 	return (0);
 }
+
+int
+cap_rights_limit(void)
+{
+	return (0);
+}
